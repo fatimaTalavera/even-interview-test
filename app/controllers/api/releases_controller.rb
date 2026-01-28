1d174ea1863f7fd26d3115fea73945282452a19c
fatimaTalavera <fa.talavera95@gmail.com>
2026-01-28 16:03:04 -0300

    implement releases index endpoint with filtering and pagination

--- app/controllers/api/releases_controller.rb
+++ app/controllers/api/releases_controller.rb
@@ -1,6 +1,13 @@
 class Api::ReleasesController < ApplicationController
   def index
-    render json: { data: [] }
+    result = ReleasesQuery.new(
+      params: params.permit(:filter, :page, :limit)
+    ).call
+
+    render json: {
+      data: result[:records].map { |release| serialize_release(release) },
+      pagination: result[:meta]
+    }
   end
 
   def create
@@ -9,4 +16,22 @@ class Api::ReleasesController < ApplicationController
 
   private
 
+  def serialize_release(release)
+    {
+      id: release.id,
+      name: release.name,
+      released_at: release.released_at.to_date,
+      album: {
+        id: release.album.id,
+        name: release.album.name,
+        duration_in_minutes: release.album.duration_in_minutes
+      },
+      artists: release.artists.map do |artist|
+        {
+          id: artist.id,
+          name: artist.name
+        }
+      end
+    }
+  end  
 end
