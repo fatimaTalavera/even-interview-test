2b4a8ff53e6a8f84fe6b5d9e5bde8dc558f3a3cf
fatimaTalavera <fa.talavera95@gmail.com>
2026-01-30 19:11:40 -0300

    add improve in json

--- app/controllers/api/releases_controller.rb
+++ app/controllers/api/releases_controller.rb
@@ -5,9 +5,13 @@ class Api::ReleasesController < ApplicationController
     ).call
 
     render json: {
+      links: {
+        self: api_releases_url
+      },
       data: result[:records].map { |release| serialize_release(release) },
+      included: result[:records].map { |release| serialize_album(release.album) }.compact + result[:records].map { |release| serialize_artists(release.artists) }.compact,
       pagination: result[:meta]
-    }
+      }
   end
 
   def create
@@ -18,20 +22,49 @@ class Api::ReleasesController < ApplicationController
 
   def serialize_release(release)
     {
-      id: release.id,
-      name: release.name,
-      released_at: release.released_at.to_date,
-      album: {
-        id: release.album.id,
-        name: release.album.name,
-        duration_in_minutes: release.album.duration_in_minutes
+      type: "release",
+      id: release.id.to_s,
+      attributes: {
+        name: release.name,
+        released_at: release.released_at.to_date
       },
-      artists: release.artists.map do |artist|
-        {
-          id: artist.id,
-          name: artist.name
+      relationships: {
+        album: {
+          links: {
+            self: "self_link",
+            related: "api_album_url(release.album)"
+          },
+          data: release.album.present? ? {
+            type: "albums",
+            id: release.album.id.to_s
+          } : nil
+        },
+        artists: {
+          links: {
+            self: "self_link",
+            related: "api_release_artists_url(release)"
+          },
+          data: release.artists.map { |artist| { type: "artists", id: artist.id.to_s } }
         }
-      end
+      },
+      links: {
+        self: "api_release_url(release)"
+      }
     }
   end  
+
+  def serialize_album(album)
+    {
+      type: "album",
+      id: album.id.to_s,
+      attributes: {
+        name: album.name,
+        duration_in_minutes: album.duration_in_minutes
+      }
+    }
+  end
+
+  def serialize_artists(artists)
+    artists.map { |artist| { type: "artist", id: artist.id.to_s, attributes: { name: artist.name } } }
+  end
 end
